8e9be3f8e8612f0f5c7fe5f6ccc00547087d81fa
bnwlkr <bnwlkr@icloud.com>
2021-12-01 11:25:55 -0800

    Changed output format to be single column of results (no longer classifying by category).

--- search.py
+++ search.py
@@ -29,22 +29,17 @@ def main():
         vids = list(csv.reader(vid_csv))
 
     with open(args.k) as kwrd_csv:
-        kwrds = list(csv.reader(kwrd_csv))
+        kwrds = [k for row in list(csv.reader(kwrd_csv))[1:] 
+                 for k in row if k != '']
 
     # some processing of vid/keyword data
 
     vids = [l[0] for l in vids]
 
-    keywords = {c:[] for c in kwrds[0]}
-    for r in kwrds[1:]:
-        for i in range(len(kwrds[0])):
-            if r[i]:
-                keywords[kwrds[0][i]].append(r[i])
-
     # get vid transcripts from youtube api
 
     transcripts = {}
-    for vid in vids:
+    for vid in vids[0:1]:
         try:
             t = YouTubeTranscriptApi.get_transcript(vid) 
         except TranscriptsDisabled:
@@ -59,31 +54,30 @@ def main():
 
     # find keyword matches in transcripts and format output rows
 
-    results = {c:[] for c in keywords.keys()}
-    for category in keywords.keys():
-        for vid,t in transcripts.items():
-            for i,t_unit in enumerate(t):
-                for keyword in keywords[category]:
-                    regex = fr'\b\w*({keyword})\w*\b'
-                    res = t_unit['text']
-                    timestamp = int(t_unit['start']) - 5
-                    found = False
-                    for match in re.finditer(regex, t_unit['text'], re.IGNORECASE):
-                        found = True
-                        res = (res[0:match.start()] + 
-                               res[match.start():match.end()].upper() +
-                               res[match.end():])
-                    if found:
-                        link = (f"=HYPERLINK(\"http://youtu.be/{vid}?t={timestamp}\","
-                                f"\"{res}\")")
-                        results[category].append(link)
+    results = []
+    for vid,t in transcripts.items():
+        for i,t_unit in enumerate(t):
+            for keyword in kwrds:
+                regex = fr'\b\w*({keyword})\w*\b'
+                res = t_unit['text']
+                timestamp = int(t_unit['start']) - 5
+                found = False
+                for match in re.finditer(regex, t_unit['text'], re.IGNORECASE):
+                    found = True
+                    res = (res[0:match.start()] + 
+                           res[match.start():match.end()].upper() +
+                           res[match.end():])
+                if found:
+                    link = (f"=HYPERLINK(\"http://youtu.be/{vid}?t={timestamp}\","
+                            f"\"{res}\")")
+                    results.append(link)
 
     # write the results to a file
 
     with open("results.csv", "w") as result_file:
        writer = csv.writer(result_file)
-       writer.writerow(results.keys())
-       writer.writerows(list(itertools.zip_longest(*results.values())))
+       for r in results:
+           writer.writerow([r])
 
                         
 
